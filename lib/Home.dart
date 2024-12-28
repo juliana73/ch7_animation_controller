8de49e042dac5120ff53e83b6d659206355042cd
juliana73 <julianaordono@gmail.com>
2024-12-28 14:19:55 -0800

    Initial Commit

---
+++ lib/Home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:ch7_animation_controller/widgets/animated_balloon.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState () => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimationController'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: SafeArea (
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column (
+              children: <Widget> [
+                AnimatedBalloonWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
